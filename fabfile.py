0df3be262b38a42014a15e58d0a46db2f076ef18
pdc <pdc@alleged.org.uk>
2012-11-02 18:15:43 +0000

    Update requorements to include psychopg

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,21 @@
 # -*-coding: UTF-8 -*-
 # Run these commands with fab
 
-from fabric.api import local, settings, abort, run, cd, env
+from fabric.api import local, settings, abort, run, cd, env, sudo, prefix
 from fabric.contrib.console import confirm
 
-env.hosts = ['caption@spreadsite.org']
+env.site_name = 'caption'
+env.hosts = ['{0}@spreadsite.org'.format(env.site_name)]
+env.virtualenv = env.site_name
+env.settings_subdir = env.site_name
+env.django_apps = ['articles']
+
+def update_requirements():
+    local("pip freeze | egrep -v 'Fabric|pycrypto|ssh' > REQUIREMENTS")
 
 def test():
     with settings(warn_only=True):
-        result = local('./manage.py test articles', capture=True)
+        result = local('./manage.py test {0}'.format(' '.join(env.django_apps)), capture=True)
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
 
@@ -19,7 +26,17 @@ def deploy():
     test()
     push()
 
-    code_dir = '/home/caption/Sites/caption'
+    run('if [ ! -d static ]; then mkdir static; fi')
+    #run('mkdir -p caches/httplib2')
+    run('mkdir -p caches/django')
+
+    code_dir = '/home/{0}/Sites/{0}'.format(env.site_name)
     with cd(code_dir):
         run('git pull')
-        run('. ~/virtualenvs/bootstrap/bin/activate && ./manage.py collectstatic --noinput')
+        run('cp {0}/settings_production.py {0}/settings.py'.format(env.settings_subdir))
+
+        with prefix('. /home/{0}/virtualenvs/{1}/bin/activate'.format(env.site_name, env.virtualenv)):
+            run('pip install -r REQUIREMENTS')
+            run('./manage.py collectstatic --noinput')
+
+    run('touch /etc/uwsgi/emperor.d/{0}.ini'.format(env.site_name))
